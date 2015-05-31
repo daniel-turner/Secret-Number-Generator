49d0a65bc196fcef7342b38221cfb2ff2534b6e2
danturne <danturne@yahoo.com>
2015-05-30 14:19:07 -1000

    completed exercise

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+
+  var randomNumber = Math.random() * 1000000;
+
+  var generateRandomNumber = function() {
+
+    return randomNumber;
+  };
+
+  return generateRandomNumber;
 };
